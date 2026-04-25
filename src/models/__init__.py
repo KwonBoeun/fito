5a407b7c48b29b00c7f40ba4e080e6627ebfdb06
LEEHAEUN0820\aoo76 <leehaeun0820@gmail.com>
2026-04-25 16:18:04 +0900

    수정

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,9 @@
 # src/models/__init__.py
 
-from .user import User, WorkoutLog
+if __name__ == "__main__":
+    raise SystemExit("이 파일은 직접 실행하지 말고 프로젝트 루트에서 app.py를 실행하세요.")
+
+from .user import User
+from .workout_logs import WorkoutLog
 
 __all__ = ["User", "WorkoutLog"]
