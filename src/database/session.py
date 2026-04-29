23958d77b58efaf6f7b96823fb6bf3a585e3f6be
DESKTOP-IU47BKE\kboeu <kwonbe2004@gmail.com>
2026-04-29 18:33:35 +0900

    충돌 해결3

--- src/database/session.py
+++ src/database/session.py
@@ -20,24 +20,14 @@ SessionLocal = sessionmaker(bind=engine, autoflush=False, expire_on_commit=False
 
 
 def init_db() -> None:
-    import src.models.group  # noqa: F401
-    import src.models.user  # noqa: F401
-<<<<<<< HEAD
-    import src.models.question         # noqa: F401
+    import src.models.group         # noqa: F401
+    import src.models.user          # noqa: F401
+    import src.models.workout_logs  # noqa: F401
+    import src.models.weight_log    # noqa: F401
+    import src.models.question      # noqa: F401
     import src.models.question_answer  # noqa: F401
-    import src.models.hashtag          # noqa: F401
+    import src.models.hashtag       # noqa: F401
 
-<<<<<<< HEAD
-    import src.models.workout_logs  # noqa: F401
-    import src.models.weight_log   # noqa: F401
-    
-=======
-=======
-    import src.models.workout_logs  # noqa: F401
-    import src.models.weight_log   # noqa: F401
-    
->>>>>>> 887d3469e292a3a84c03097ce20ad8e0af28f716
->>>>>>> BoEun
     Base.metadata.create_all(bind=engine)
     _ensure_schema_updates()
 
